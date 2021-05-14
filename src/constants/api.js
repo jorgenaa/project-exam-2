16f264286dd5aa2c2a5bb5acc48e538a729736cc
Jørgen Årnes <36748061+jorgenaa@users.noreply.github.com>
2021-05-14 19:16:29 +0200

    test

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,5 +1,5 @@
-export const BASE_URL = process.env.REACT_APP_BASE_URL;
-
+//export const BASE_URL = process.env.REACT_APP_BASE_URL;
+export const BASE_URL = process.env.NODE_ENV === "production" ? "https://strapi-api-holidaze.herokuapp.com" : "http://localhost:1337"; 
 export const TOKEN_PATH = '/auth/local';
 
 export const HOTEL_PATH = '/establishments';
